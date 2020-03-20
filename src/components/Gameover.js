6eca0693b44f6f74f345e68e1a31c08222c761b9
JadowityHerbatnik <maciej.mancewicz@gmail.com>
2020-03-20 19:06:50 +0100

    Animation on mount/unmount

--- src/components/Gameover.js
+++ src/components/Gameover.js
@@ -1,31 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../fontello/css/fontello.css";
 
-class GameOver extends React.Component {
-  render() {
-    let style = {};
-    if (this.props.winner) {
-      style.opacity = 1;
-      style.pointerEvents = "auto";
-      style.transitionDelay = "2s";
+const GameOver = ({ winner, resetGame }) => {
+  const [shouldRender, setRender] = useState(winner);
+  const [style, setStyle] = useState({ opacity: "0" });
+
+  useEffect(() => {
+    if (winner) {
+      setRender(true);
+      setStyle({ animation: "1s ease both 2s fadein" });
+    } else {
+      setStyle({ animation: "1s ease both fadeout" });
     }
-    return (
-      <div id="gameOver" style={style}>
-        <p id="gameOverText">
-          You
-          <br />
-          Loose
-        </p>
-        <button
-          id="resetButton"
-          className="but"
-          onClick={() => this.props.onClick()}
-        >
-          <i className="icon-ccw"></i>
-          Try Again
-        </button>
-      </div>
-    );
-  }
-}
+  }, [winner]);
+  const onAnimationEnd = () => {
+    if (!winner) {
+      setRender(false);
+    }
+  };
+  return !shouldRender ? null : (
+    <div id="gameOver" onAnimationEnd={onAnimationEnd} style={style}>
+      <p id="gameOverText">
+        You
+        <br />L<span>ooo</span>se
+      </p>
+      <button id="resetButton" className="but" onClick={() => resetGame()}>
+        <i className="icon-ccw"></i>
+        Try Again
+      </button>
+    </div>
+  );
+};
 export default GameOver;
