f4b80ae01cdca05e236d167612ba13ddd0d16315
JadowityHerbatnik <maciej.mancewicz@gmail.com>
2020-03-22 17:40:51 +0100

    Freindship ended with fontello, now svg is my best friend

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,20 +1,57 @@
 import React from "react";
-import "../fontello/css/fontello.css";
 
+const O_tic = props => {
+  return (
+    <svg width={props.svgSize} height={props.svgSize}>
+      <circle
+        className="circle"
+        cx={props.svgSize / 2}
+        cy={props.svgSize / 2}
+        strokeWidth={props.svgSize / 13}
+        strokeDasharray={(props.svgSize / 4) * 6.28}
+        strokeDashoffset={(props.svgSize / 4) * 6.28}
+        r={props.svgSize / 4}
+        transform={`rotate(-90, ${props.svgSize / 2}, ${props.svgSize / 2})`}
+      />
+    </svg>
+  );
+};
+const Line = (props, rotated) => {
+  return (
+    <line
+      className="xline"
+      id={rotated ? "rotated" : null}
+      x1="0"
+      y1={props.xSize / 2}
+      x2={props.xSize}
+      y2={props.xSize / 2}
+      strokeWidth={props.xSize / 8}
+      strokeDasharray={props.xSize}
+      strokeDashoffset={props.xSize}
+      transform={`rotate(${rotated ? 135 : 45}, ${props.xSize /
+        2}, ${props.xSize / 2}) `}
+    />
+  );
+};
+const X_tic = props => {
+  return (
+    <svg width={props.xSize} height={props.xSize}>
+      {Line(props, false)}
+      {Line(props, true)}
+    </svg>
+  );
+};
 const Square = props => {
-  const iconClassName = `${
-    props.value === null
-      ? null
-      : `icon-${props.value === "X" ? "cancel-1" : "record-outline"}`
-  }`;
+  const active = props.value;
   return (
     <button
       className="square"
-      style={props.squareStyle}
+      style={{ width: props.squareSize, height: props.squareSize }}
       onClick={props.clickOnSquare}
       aria-label={props.arialabel}
     >
-      {props.value && <i className={iconClassName} style={props.fontSize}></i>}
+      {active === "X" && X_tic(props)}
+      {active === "O" && O_tic(props)}
     </button>
   );
 };
@@ -28,14 +65,13 @@ const Board = React.forwardRef((props, ref) => {
       <Square
         value={props.squares[i]}
         clickOnSquare={() => props.clickOnSquare(i)}
-        squareStyle={{ width: squareSize, height: squareSize }}
-        fontSize={{ fontSize: squareSize / 1.5 }}
+        squareSize={squareSize}
+        svgSize={squareSize / 1.2}
+        xSize={squareSize / 2}
         arialabel={`board field no. ${i + 1}`}
       />
     );
   };
-  const generateRows = num =>
-    [...Array(num).keys()].map(index => RenderRow(index * 3));
 
   const RenderRow = index => {
     for (let i = index; i < index + 3; i++) {
@@ -52,7 +88,7 @@ const Board = React.forwardRef((props, ref) => {
   return (
     <div ref={props.boardref} className="game">
       <div className="board">
-        {generateRows(3)}
+        {[...Array(3).keys()].map(index => RenderRow(index * 3))}
         <div className="gridLines"></div>
         <div className="gridLines rotate"></div>
         <div className="line" style={props.lineStyle} />
